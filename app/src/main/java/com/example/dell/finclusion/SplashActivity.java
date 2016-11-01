c1a8b5834e2500948e3f91080edac00a5d38e36f
Ashu Gupta <ashu10832@gmail.com>
2016-11-01 14:29:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dell/finclusion/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.example.dell.finclusion;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    public static final int DELAYED_TIME = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        },DELAYED_TIME);
+    }
+}
